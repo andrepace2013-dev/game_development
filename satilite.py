b226f1c9cb12a97456c1e482660ccda7e58eb4e6
andrepace2013-dev <andre.pace.2013@gmail.com>
2026-08-03 16:55:07 +0200

    Add files via upload

---
+++ satilite.py
@@ -0,0 +1,37 @@
+import pgzrun
+import time
+import random
+
+WIDTH = 800
+HEIGHT = 600
+satellites = []
+number_of_satellite = 8
+next_satellite = 0
+
+lines = []
+
+start_time = 0
+total_time = 0
+end_time = 0
+
+def create_satellite():
+    global start_time
+    for i in range(number_of_satellite):
+        satellite = Actor("satellite")
+        satellite.pos = random.randint(50,750),random.randint(50,550)
+        satellites.append(satellite)
+    start_time = time.time()
+create_satellite()
+
+def draw():
+    global total_time
+    screen.blit("sky",(0,0))
+    number = 1
+    for satellite in satellites:
+        satellite.draw()
+        screen.draw.text(str(number),(satellite.pos[0],satellite.pos[1]+20))
+        number+=1
+    for line in lines:
+        screen.draw.line(line[0], line[1], (255,255,255))
+
+pgzrun.go()
